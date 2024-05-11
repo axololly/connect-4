da289e645273aafd9b8616dc290a5ea3c794ad9c
axololly <jamesgriggs362@gmail.com>
2024-05-11 16:31:44 +0100

    Fixed bitboards, added heuristic, added negamax.

---
+++ bitboard.py
@@ -0,0 +1,106 @@
+from typing import List, NewType, Self
+from copy import deepcopy
+
+Binary = NewType('Binary', int)
+
+class Bitboard:
+    STARTING_POSITION = [[0 for _ in range(7)] for _ in range(6)]
+
+    def __init__(self, *bitboards: Binary):
+        self.bitboards: List[Binary] = list(bitboards)
+
+        if not self.bitboards:
+            self.bitboards = [0, 0]
+        
+        if len(self.bitboards) < 2:
+            self.bitboards.append(0)
+        
+        if len(self.bitboards) > 2:
+            raise Exception("More than two bitboards were parsed as arguments.")
+        
+        self.heights = [n * 7 for n in range(7)]
+        self.counter = 0
+        self.moves = []
+    
+    def __repr__(self) -> str:
+        state = []
+
+        for i in range(6):
+            row_str = ''
+
+            for j in range(7):
+                pos = 1 << 7 * j + i
+            
+                if self.bitboards[0] & pos == pos:
+                    row_str += '1 '
+                elif self.bitboards[1] & pos == pos:
+                    row_str += '2 '
+                else:
+                    row_str += '. '
+            
+            state.append(row_str)
+        
+        state.reverse()
+        
+        return '\n'.join(state)
+    
+    def makeMove(self, column: int) -> None:
+        self.bitboards[self.counter % 2] ^= 1 << self.heights[column]
+        self.heights[column] += 1
+        self.moves.append(column)
+        self.counter += 1
+        
+        """
+        move = 1 << self.heights[column]
+        self.heights[column] += 1  # (1)
+        self.bitboards[self.counter & 1] ^= move  # (2)
+        self.moves.append(column)  # (3)
+        self.counter += 1
+        """
+
+    def undoMove(self) -> None:
+        col = self.moves.pop()
+        self.bitboards[self.counter % 2] ^= 1 << self.heights[col]
+        self.counter -= 1
+        self.heights[col] -= 1
+
+    def isWin(self) -> bool:
+        bitboard = self.bitboards[(self.counter - 1) % 2]
+
+        directions = [1, 7, 6, 8]
+        bb = None
+
+        for direction in directions:
+            bb = bitboard & (bitboard >> direction)
+
+            if bb & (bb >> (2 * direction)) != 0:
+                return True
+        
+        return False
+
+    def getNextMoves(self) -> List[int]:
+        moves: List[int] = []
+        TOP = 0b1000000_1000000_1000000_1000000_1000000_1000000_1000000
+
+        for i in range(7):
+            if TOP & (1 << self.heights[i]) == 0:
+                moves.append(i)
+        
+        return moves
+
+    def is_winning_move(self, move: int) -> bool:
+        separate_board = self.copy()
+        separate_board.makeMove(move)
+        is_winning = separate_board.isWin()
+        separate_board.undoMove()
+        
+        return is_winning
+
+    def copy(self) -> Self:
+        return deepcopy(self)
+    
+    def clear(self) -> None:
+        self.bitboards = [0, 0]
+        self.counter = 0
+        self.moves = []
+        self.heights = [n * 7 for n in range(7)]
--- negamax.py
+++ negamax.py
@@ -1,138 +1,60 @@
-from enum import Enum
-from game import Game, Outcomes
-from typing import List, Tuple
-from copy import deepcopy
+from bitboard import Bitboard
+from heuristic import Heuristic
 
-class Results(Enum):
-    WIN: int = 1
-    DRAW: int = 0
-    LOSE: int = -1
+def negamax(board: Bitboard, depth: int, alpha: int, beta: int) -> int:
+    board = board.copy()
 
-class Players(Enum):
-    P1: int = 1
-    P2: int = -1
+    if depth == 0:
+        return Heuristic().heuristic(board)
 
-class Negamax:
-    def __init__(self, board: List[List[int]]):
-        self.board = board
-        self.iterations = 0
+    if board.counter == 42:
+        return 0
     
-    def contains(self, sublist: List, lst: List) -> bool:
-        return any(sublist == lst[i : i + len(sublist)] for i in range(len(lst) - len(sublist) + 1))
-
-    def heuristic(self, board: List[List[int]]) -> int | float:
-        if board == [[[0] for _ in range(6)] for _ in range(7)]:
-            return 0
-
-        outcome = Game(board).is_game_over()
-
-        if outcome is Outcomes.P1_WIN:
-            return 1
-        elif outcome is Outcomes.DRAW:
-            return 0
-        elif outcome is Outcomes.P2_WIN:
-            return -1
-        
-        my_lines_of_nearly_3 = 0
-        opp_lines_of_nearly_3 = 0
-
-        my_lines_of_nearly_4 = 0
-        opp_lines_of_nearly_4 = 0
-
-        rotated_board = [
-            [
-                board[-1 - i][x] for i, _ in enumerate(board)
-            ]
-            for x, _ in enumerate(self.board[0])
-        ]
-
-        # vertical and horizontal
-        for board in [board, rotated_board]:
-            for row in board:
-                for i in range(4):
-                    line = row[i : i + 4]
-                    
-                    my_lines_of_nearly_3 += 1 if self.contains([0, 1, 1], line) else 0
-                    my_lines_of_nearly_3 += 1 if self.contains([1, 1, 0], line) else 0
-
-                    opp_lines_of_nearly_3 += 1 if self.contains([0, 2, 2], line) else 0
-                    opp_lines_of_nearly_3 += 1 if self.contains([2, 2, 0], line) else 0
-
-                    my_lines_of_nearly_4 += 1 if self.contains([0, 1, 1, 1], line) else 0
-                    my_lines_of_nearly_4 += 1 if self.contains([1, 1, 1, 0], line) else 0
-
-                    opp_lines_of_nearly_4 += 1 if self.contains([0, 2, 2, 2], line) else 0
-                    opp_lines_of_nearly_4 += 1 if self.contains([2, 2, 2, 0], line) else 0
-        
-        for x_change in (1, -1):
-            for i in range(3):
-                for j in range(3):
-                    line = [board[i + x][j + x * x_change] for x in range(4)]
-                        
-                    my_lines_of_nearly_3 += 1 if self.contains([0, 1, 1], line) else 0
-                    my_lines_of_nearly_3 += 1 if self.contains([1, 1, 0], line) else 0
-
-                    opp_lines_of_nearly_3 += 1 if self.contains([0, 2, 2], line) else 0
-                    opp_lines_of_nearly_3 += 1 if self.contains([2, 2, 0], line) else 0
-
-                    my_lines_of_nearly_4 += 1 if self.contains([0, 1, 1, 1], line) else 0
-                    my_lines_of_nearly_4 += 1 if self.contains([1, 1, 1, 0], line) else 0
-        
-        return 1.75 * (my_lines_of_nearly_4 - opp_lines_of_nearly_4) / 7 + 1.25 * (my_lines_of_nearly_3 - opp_lines_of_nearly_3) / 7
-
-    def get_children(self, board: List[List[int]], *, player: int) -> Tuple[int, List[List[int]]]:
-        children = []
-
-        for move in Game(board).valid_moves():
-            new_board = deepcopy(board)
-            Game(new_board).place_counter(move, player)
-            children.append((move, new_board))
-        
-        return children
-
-    def negamax(self, state: List[List[int]], depth: int, alpha: int, beta: int) -> int | float:
-        self.iterations += 1
-        game = Game(state)
-        
-        if depth == 0 or game.is_game_over() != Outcomes.NONE:
-            return self.heuristic(state)
-
-        value = float('-inf')
-
-        for _, position in self.get_children(game.board, player = 1 if self.min_or_max(state) == Players.P1 else 2):
-            value = max(value, -self.negamax(position, depth - 1, alpha, beta))
-            alpha = max(alpha, value)
-                
-            if beta <= alpha:
-                break
-            
-        return value
+    if board.isWin():
+        # print("winning board:")
+        # print(repr(board))
+        return (42 + 1 - board.counter) // 2 * (-1 if board.counter + 1 & 1 else 1)
     
-    def min_or_max(self, state: List[List[int]] = None):
-        state = state or self.board
-
-        count_1s = sum(row.count(1) for row in state)
-        count_2s = sum(row.count(2) for row in state)
-
-        if count_1s - count_2s not in (1, 0, -1):
-            raise Exception(f"Invalid board state. Player 1 has {count_1s} counters and Player 2 has {count_2s} counters.")
-
-        return Players.P1 if count_1s == count_2s else Players.P2
+    for x in board.getNextMoves():
+        if board.is_winning_move(x):
+            return (42 + 1 - board.counter) // 2 * (-1 if board.counter & 1 else 1)
     
-    def get_best_move(self, state: List[List[int]] = None, depth: int = 10) -> int:
-        state = state or self.board
-        
-        best_move = None
-        
-        max_eval = float('-inf')
+    max_score = (42 - 1 - board.counter) // 2
+    
+    if beta > max_score:
+        beta = max_score
 
-        for move, next_position in self.get_children(state, player = 1 if self.min_or_max(state) == Players.P1 else 2):
-            evaluation = -self.negamax(next_position, depth, float('-inf'), float('inf'))
-
-            print(move, evaluation)
-
-            if evaluation > max_eval:
-                max_eval = evaluation
-                best_move = move
+        if alpha >= beta:
+            return beta
+    
+    for x in board.getNextMoves():
+        board.makeMove(x)
+        score = -negamax(board, depth - 1, -beta, -alpha)
+        board.undoMove()
         
-        return best_move
+        alpha = max(alpha, score)
+    
+    return alpha
+
+def get_best_move(board: Bitboard, /, depth: int = 10) -> int:
+    board = board.copy()
+    best_score = -float('inf')
+    best_move = 0
+
+    all_scores = {}
+
+    for col in board.getNextMoves():
+        board.makeMove(col)
+        score = -negamax(board, depth, float('-inf'), float('inf'))
+        board.undoMove()
+
+        all_scores[col] = score
+
+        # print(score, col)
+
+        if score > best_score:
+            best_score = score
+            best_move = col
+    
+    print("All scores: ", all_scores)
+    return best_move
